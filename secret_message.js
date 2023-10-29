2dcd754380f947584f5b8b2430ee88c41a906453
nsLittle <144547014+nsLittle@users.noreply.github.com>
2023-10-29 07:52:04 -0400

    Create secret_message.js

---
+++ secret_message.js
@@ -0,0 +1,14 @@
+let secretMessage = ['Learning', 'is', 'not', 'about', 'what', 'you', 'get', 'easily', 'the', 'first', 'time,', 'it', 'is', 'about', 'what', 'you', 'can', 'figure', 'out.', '-2015,', 'Chris', 'Pine,', 'Learn', 'JavaScript'];
+// console.log(secretMessage.length);
+secretMessage.pop();
+// console.log(secretMessage.length);
+secretMessage.push("to");
+secretMessage.push("Program");
+// console.log(secretMessage.indexOf("easily"));
+secretMessage.splice(7, 1, "right");
+secretMessage.shift();
+secretMessage.unshift("Programming");
+// console.log(secretMessage.indexOf("get"));
+secretMessage.splice(6, 5, "know");
+console.log(secretMessage);
+console.log(secretMessage.join(" "));
